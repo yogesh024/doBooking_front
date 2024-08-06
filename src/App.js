057e86a44b86e8b96a085fedf4ea7c89e287d575
yogesh024 <ykajay55@gmail.com>
2024-08-06 11:15:10 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
+
+export const URL=process.env.REACT_APP_SERVER_URL;
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/hotels" element={<List/>}/>
+        <Route path="/hotels/:id" element={<Hotel/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
